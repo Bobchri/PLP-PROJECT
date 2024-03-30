81508cebb5837fe78a01efc338def496a2739af9
Bobchri <125733720+Bobchri@users.noreply.github.com>
2024-03-30 18:09:18 +0300

    user_input.py

---
+++ PYTHON/user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
